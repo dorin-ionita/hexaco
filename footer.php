ebde1db7cd328a3642fef06fe2433ac561a0eac9
Dorin Ionita <dorin.marian.ionita@gmail.com>
2016-10-19 23:42:19 +0300

    fixed_footer_%broken_links

--- footer.php
+++ footer.php
@@ -2,10 +2,10 @@
 	echo "	<div id=\"footer\">
 		<div>
 			<div class=\"connect\">
-				<a href=\"http://freewebsitetemplates.com/go/twitter/\" id=\"twitter\">twitter</a>
-				<a href=\"http://freewebsitetemplates.com/go/facebook/\" id=\"facebook\">facebook</a>
-				<a href=\"http://freewebsitetemplates.com/go/googleplus/\" id=\"googleplus\">googleplus</a>
-				<a href=\"http://pinterest.com/fwtemplates/\" id=\"pinterest\">pinterest</a>
+				<a href=\"index.php" id=\"twitter\">twitter</a>
+				<a href=\"index.php\" id=\"facebook\">facebook</a>
+				<a href=\"index.php\" id=\"googleplus\">googleplus</a>
+				<a href=\"index.php\" id=\"pinterest\">pinterest</a>
 			</div>
 			<p>
 				&copy; copyright 2016 | all rights reserved.
