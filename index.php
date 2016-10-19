662d207f01d33bcf1b5faf4016976667cb4a08af
Dorin Ionita <dorin.marian.ionita@gmail.com>
2016-10-19 23:27:06 +0300

    included_php_for_header_footer_and_head_for_real_now

--- index.php
+++ index.php
@@ -1,31 +1,11 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<title>Hexaco PI</title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<?php include "head.php";?>
 </head>
 <body>
 	<div id="header">
-		<div class="section">
-			<div class="logo">
-				<a href="index.html">Hexaco</a>
-			</div>
-			<ul>
-				<li class="selected">
-					<a href="index.html">home</a>
-				</li>
-				<li>
-					<a href="testul.html">testul</a>
-				</li>
-				<li>
-					<a href="rezultate.html">rezultate</a>
-				</li>
-				<li>
-					<a href="contact.html">contact</a>
-				</li>
-			</ul>
-		</div>
+		<?php include "antent.php";?>
 		<div class="article">
 			<img src="images/home_image.png" alt="">
 			<h1>
@@ -35,18 +15,6 @@
 				Dumitrescu Eduard
 			</h1>
 		</div>
-	<div id="footer">
-		<div>
-			<div class="connect">
-				<a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a>
-				<a href="http://freewebsitetemplates.com/go/facebook/" id="facebook">facebook</a>
-				<a href="http://freewebsitetemplates.com/go/googleplus/" id="googleplus">googleplus</a>
-				<a href="http://pinterest.com/fwtemplates/" id="pinterest">pinterest</a>
-			</div>
-			<p>
-				&copy; copyright 2016 | all rights reserved.
-			</p>
-		</div>
-	</div>
+		<?php include "footer.php";?>
 </body>
 </html>
--- rezultate.php
+++ rezultate.php
@@ -2,34 +2,11 @@
 <!-- Website Template by freewebsitetemplates.com -->
 <html>
 <head>
-	<meta charset="UTF-8">
-	<title>News - ZiZurz Website Template</title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<?php include "head.php";?>
 </head>
 <body>
 	<div id="header">
-		<div class="section">
-			<div class="logo">
-				<a href="index.html">zizurz</a>
-			</div>
-			<ul>
-				<li>
-					<a href="index.html">home</a>
-				</li>
-				<li>
-					<a href="about.html">about</a>
-				</li>
-				<li>
-					<a href="hairstyle.html">hairstyles</a>
-				</li>
-				<li class="selected">
-					<a href="news.html">news</a>
-				</li>
-				<li>
-					<a href="contact.html">contact</a>
-				</li>
-			</ul>
-		</div>
+		<?php include "antent.php";?>
 	</div>
 	<div id="body">
 		<div class="blog">
@@ -70,18 +47,6 @@
 			</div>
 		</div>
 	</div>
-	<div id="footer">
-		<div>
-			<div class="connect">
-				<a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a>
-				<a href="http://freewebsitetemplates.com/go/facebook/" id="facebook">facebook</a>
-				<a href="http://freewebsitetemplates.com/go/googleplus/" id="googleplus">googleplus</a>
-				<a href="http://pinterest.com/fwtemplates/" id="pinterest">pinterest</a>
-			</div>
-			<p>
-				&copy; copyright 2023 | all rights reserved.
-			</p>
-		</div>
-	</div>
+	<?php include "footer.php";?>
 </body>
 </html>
