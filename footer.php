1dcf7ff512fd4bd432a9b8401585006da3c3f9a5
Dorin Ionita <dorin.marian.ionita@gmail.com>
2016-10-19 23:35:35 +0300

    fixed_footer_%the_problem_with_twitter%

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 	echo "	<div id=\"footer\">
 		<div>
 			<div class=\"connect\">
-				<a href=\"http://freewebsitetemplates.com/go/twitter/\" id="twitter">twitter</a>
+				<a href=\"http://freewebsitetemplates.com/go/twitter/\" id=\"twitter\">twitter</a>
 				<a href=\"http://freewebsitetemplates.com/go/facebook/\" id=\"facebook\">facebook</a>
 				<a href=\"http://freewebsitetemplates.com/go/googleplus/\" id=\"googleplus\">googleplus</a>
 				<a href=\"http://pinterest.com/fwtemplates/\" id=\"pinterest\">pinterest</a>
