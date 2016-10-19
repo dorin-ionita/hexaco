2f6366feae7475c14359ae9b62facea11b9c6ce0
Dorin Ionita <dorin.marian.ionita@gmail.com>
2016-10-19 23:43:40 +0300

    fixed_footer_%broken_links

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 	echo "	<div id=\"footer\">
 		<div>
 			<div class=\"connect\">
-				<a href=\"index.php" id=\"twitter\">twitter</a>
+				<a href=\"index.php\" id=\"twitter\">twitter</a>
 				<a href=\"index.php\" id=\"facebook\">facebook</a>
 				<a href=\"index.php\" id=\"googleplus\">googleplus</a>
 				<a href=\"index.php\" id=\"pinterest\">pinterest</a>
